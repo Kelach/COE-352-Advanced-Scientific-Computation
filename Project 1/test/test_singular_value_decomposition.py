af165f422618a21bb79577e7192b878200f35c7f
Kelechi Emeruwa <kelechi@utexas.edu>
2023-09-13 22:40:36 -0500

    added test file

---
+++ Project 1/test/test_singular_value_decomposition.py
@@ -0,0 +1,8 @@
+import pytest
+import numpy as np
+def test_singular_value_decomposition():
+    test_matrix1 = np.array([[5, -1],[5, 7]])
+    test_matrix2 = np.array([[3, 3, 2],[2, 3, -2]])
+    # test_matrix3 = np.array([[5, -1],[5, 7]])
+
+    pass
